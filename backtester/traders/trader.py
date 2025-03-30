97c56d89c0e4b2721635d6aebb52879d3b2dc2cb
Edward Wei <ewei2406@gmail.com>
2025-03-29 23:00:54 -0400

    backtester stub

---
+++ backtester/traders/trader.py
@@ -0,0 +1,17 @@
+from datamodel import Order, TradingState
+
+
+class Trader:
+    def run(self, state: TradingState):
+        result = {}
+        current_time = state.timestamp
+        best_ask, best_ask_amount = list(
+            state.order_depths["KELP"].sell_orders.items()
+        )[0]
+        best_bid, best_bid_amount = list(state.order_depths["KELP"].buy_orders.items())[
+            0
+        ]
+        if current_time == 0:
+            result["KELP"] = [Order("KELP", best_ask, 1)]
+        traderData = "SAMPLE"
+        return result, None, traderData
--- datamodel.py
+++ datamodel.py
@@ -1,6 +1,7 @@
 import json
-from typing import Dict, List
 from json import JSONEncoder
+from typing import Dict, List
+
 import jsonpickle
 
 Time = int
@@ -12,16 +13,23 @@ ObservationValue = int
 
 
 class Listing:
-
     def __init__(self, symbol: Symbol, product: Product, denomination: Product):
         self.symbol = symbol
         self.product = product
         self.denomination = denomination
-        
-                 
-class ConversionObservation:
 
-    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sugarPrice: float, sunlightIndex: float):
+
+class ConversionObservation:
+    def __init__(
+        self,
+        bidPrice: float,
+        askPrice: float,
+        transportFees: float,
+        exportTariff: float,
+        importTariff: float,
+        sugarPrice: float,
+        sunlightIndex: float,
+    ):
         self.bidPrice = bidPrice
         self.askPrice = askPrice
         self.transportFees = transportFees
@@ -29,42 +37,65 @@ class ConversionObservation:
         self.importTariff = importTariff
         self.sugarPrice = sugarPrice
         self.sunlightIndex = sunlightIndex
-        
+
 
 class Observation:
-
-    def __init__(self, plainValueObservations: Dict[Product, ObservationValue], conversionObservations: Dict[Product, ConversionObservation]) -> None:
+    def __init__(
+        self,
+        plainValueObservations: Dict[Product, ObservationValue],
+        conversionObservations: Dict[Product, ConversionObservation],
+    ) -> None:
         self.plainValueObservations = plainValueObservations
         self.conversionObservations = conversionObservations
-        
+
     def __str__(self) -> str:
-        return "(plainValueObservations: " + jsonpickle.encode(self.plainValueObservations) + ", conversionObservations: " + jsonpickle.encode(self.conversionObservations) + ")"
-     
+        return (
+            "(plainValueObservations: "
+            + jsonpickle.encode(self.plainValueObservations)
+            + ", conversionObservations: "
+            + jsonpickle.encode(self.conversionObservations)
+            + ")"
+        )
+
 
 class Order:
-
     def __init__(self, symbol: Symbol, price: int, quantity: int) -> None:
         self.symbol = symbol
         self.price = price
         self.quantity = quantity
 
     def __str__(self) -> str:
-        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        return (
+            "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        )
 
     def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
-    
+        return (
+            "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        )
+
 
 class OrderDepth:
-
-    def __init__(self):
-        self.buy_orders: Dict[int, int] = {}
-        self.sell_orders: Dict[int, int] = {}
+    def __init__(
+        self, buy_orders: Dict[int, int] = None, sell_orders: Dict[int, int] = None, _mid_price: float = 0
+    ) -> None:
+        self.buy_orders: Dict[int, int] = buy_orders if buy_orders is not None else {}
+        self.sell_orders: Dict[int, int] = (
+            sell_orders if sell_orders is not None else {}
+        )
+        self._mid_price = _mid_price # Just for the backtester
 
 
 class Trade:
-
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId=None, seller: UserId=None, timestamp: int=0) -> None:
+    def __init__(
+        self,
+        symbol: Symbol,
+        price: int,
+        quantity: int,
+        buyer: UserId = None,
+        seller: UserId = None,
+        timestamp: int = 0,
+    ) -> None:
         self.symbol = symbol
         self.price: int = price
         self.quantity: int = quantity
@@ -73,23 +104,52 @@ class Trade:
         self.timestamp = timestamp
 
     def __str__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
+        return (
+            "("
+            + self.symbol
+            + ", "
+            + self.buyer
+            + " << "
+            + self.seller
+            + ", "
+            + str(self.price)
+            + ", "
+            + str(self.quantity)
+            + ", "
+            + str(self.timestamp)
+            + ")"
+        )
 
     def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
+        return (
+            "("
+            + self.symbol
+            + ", "
+            + self.buyer
+            + " << "
+            + self.seller
+            + ", "
+            + str(self.price)
+            + ", "
+            + str(self.quantity)
+            + ", "
+            + str(self.timestamp)
+            + ")"
+        )
 
 
 class TradingState(object):
-
-    def __init__(self,
-                 traderData: str,
-                 timestamp: Time,
-                 listings: Dict[Symbol, Listing],
-                 order_depths: Dict[Symbol, OrderDepth],
-                 own_trades: Dict[Symbol, List[Trade]],
-                 market_trades: Dict[Symbol, List[Trade]],
-                 position: Dict[Product, Position],
-                 observations: Observation):
+    def __init__(
+        self,
+        traderData: str,
+        timestamp: Time,
+        listings: Dict[Symbol, Listing],
+        order_depths: Dict[Symbol, OrderDepth],
+        own_trades: Dict[Symbol, List[Trade]],
+        market_trades: Dict[Symbol, List[Trade]],
+        position: Dict[Product, Position],
+        observations: Observation,
+    ):
         self.traderData = traderData
         self.timestamp = timestamp
         self.listings = listings
@@ -98,12 +158,11 @@ class TradingState(object):
         self.market_trades = market_trades
         self.position = position
         self.observations = observations
-        
+
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
 
-    
-class ProsperityEncoder(JSONEncoder):
 
-        def default(self, o):
-            return o.__dict__
+class ProsperityEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
